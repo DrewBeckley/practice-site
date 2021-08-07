ec899dd1e93b2d4fa3668935ff97b3150060e036
Drew Beckley <85914430+DrewBeckley@users.noreply.github.com>
2021-08-07 05:52:49 +0000

    continuing to test looks

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //video I am following https://www.youtube.com/watch?v=kMT54MPz9oE
 //title: HTML and CSS Tutorial - Create a Website for Beginners
-//I will do the the same 
-//let l="Properly sized paragraphs play an important role in making your"
+//I will do the the something 
+//seeing how long the paragraph on a site was to see if  it followed the 75 char rule
+let l="Properly sized paragraphs play an important role in making your"
 console.log(l.length)
